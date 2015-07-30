d3264b5f265df3020d8145dcf99a3f3ac9520400
tbaddade <thomas@redaxo.org>
2015-07-30 22:47:05 +0200

    Initialer Commit

---
+++ install.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * YCountries Addon
+ * @author Yakamara GmbH & Co. KG
+ * @author thomas.blum@redaxo.org
+ * @package redaxo4.6
+ */
+
+$REX['ADDON']['install']['ycountries'] = true;
